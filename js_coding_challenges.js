4290791a991cd6e0df851aca75c9238d421d2af9
= <=>
2023-10-05 20:11:35 +0100

    first commit

---
+++ js_coding_challenges.js
@@ -0,0 +1,57 @@
+// wrtie a tradition function that multiplies two number and returns the value print the value outside the function
+
+function multiply(num1, num2) {
+    let result = num1 * num2
+    return result;
+}
+
+console.log(multiply(5,3))
+
+
+// write a for loop that prints your first name 10 times
+
+// for(let i =0; i< 10; i++) {
+//     console.log("ahmed")
+// }
+
+// write a while loop that prints your surname 15 times
+
+// let i =0;
+// while (i < 15) {
+//     console.log("Dini")
+//     i++
+// }
+
+// code an array of 10 element of type string (for example 10 students)
+
+// let students = ["John", "Layland", "Ali", "Bushra","Ahmed","Liban", "Fatima", "Noora", "Hidaya", "Bilal"]
+
+
+// write a for loop that iterates through each element and prints each elelement
+
+// for (let i = 0; i < students.length;i++) {
+//     console.log(students[i])
+// }
+
+// name few functions that come with arrays, that allow you to interate through the elements
+
+// students.forEach()
+// students.map()
+// students.filter()
+// students.reduce()
+
+// implement forEach function that prints all elements of the array below
+
+// students.forEach(
+//    function (value,index) {
+//       console.log("the element is " + value + " and its index is " + index)
+//    } 
+// )
+
+
+
+// arrow functions
+// switch
+// maps
+// sets
+// 
